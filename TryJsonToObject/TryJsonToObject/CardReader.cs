6e33cc5373a61d584ff057cdb291bcc2e16f6119
Altamish Mahomed <gambit318@gmail.com>
2022-01-26 23:08:20 +0200

    Refactor classes.

---
+++ TryJsonToObject/TryJsonToObject/CardReader.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using Aspose.Cells;
+using Aspose.Cells.Utility;
+using Newtonsoft.Json;
+
+namespace TryJsonToObject
+{
+  internal class JsonIntermediateCard
+  {
+    public int? Quantity { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Guild { get; set; }
+    public int? Cost { get; set; }
+    public int? Defense { get; set; }
+    public int? Shield { get; set; }
+    public string DefaultAbilities { get; set; }
+    public string GuildBonuses { get; set; }
+    public string AllyBonuses { get; set; }
+    public string ScrapBonuses { get; set; }
+    public int? Id { get; set; }
+  }
+
+  public static class CardReader
+  {
+    private static readonly string JoinedActionDelim = ",";
+
+    private static int? GetActionValue(string str, string action)
+    {
+      if (str.Contains(action) == false) return null;
+
+      //str = str.Replace(action, "");
+      var stripped = str.Substring(action.Length);
+
+      return int.Parse(stripped);
+    }
+
+    private static ActionSet ConstructActionSet(string actionString)
+    {
+      if (actionString == null) return new ActionSet();
+
+      var set = new ActionSet();
+
+      var actions = actionString.Split(JoinedActionDelim);
+
+      foreach (var action in actions)
+      {
+        int? val;
+
+        val = GetActionValue(action, Actions.Attack);
+        set.Attack = val ?? set.Attack;
+
+        val = GetActionValue(action, Actions.Draw);
+        set.Draw = val ?? set.Draw;
+
+        val = GetActionValue(action, Actions.Scrap);
+        set.Scrap = val ?? set.Scrap;
+
+        val = GetActionValue(action, Actions.OpponentDiscard);
+        set.OpponentDiscard = val ?? set.OpponentDiscard;
+
+        val = GetActionValue(action, Actions.Consume);
+        set.Consume = val ?? set.Consume;
+
+        val = GetActionValue(action, Actions.Heal);
+        set.Heal = val ?? set.Heal;
+
+        val = GetActionValue(action, Actions.Trade);
+        set.Trade = val ?? set.Trade;
+      }
+
+      return set;
+    }
+
+    private static void PopulateCardsFromJsonIntermediates(List<JsonIntermediateCard> intermediateCards, ref List<Card> cards)
+    {
+      foreach (var ic in intermediateCards)
+      {
+        var card = new Card(
+          ic.Id ?? 0,
+          ic.Name,
+          ic.Type,
+          ic.Guild,
+          ic.Cost ?? 0,
+          ic.Defense ?? 0,
+          ic.Shield ?? 0,
+          ConstructActionSet(ic.DefaultAbilities),
+          ConstructActionSet(ic.GuildBonuses),
+          ConstructActionSet(ic.AllyBonuses),
+          ConstructActionSet(ic.ScrapBonuses)
+        );
+
+        for (var i = 0; i < ic.Quantity; ++i)
+        {
+          cards.Add(card);
+        }
+      }
+    }
+
+    private static string ExcelToJson(string excelFile)
+    {
+      var workbook = new Workbook(excelFile, new LoadOptions(LoadFormat.Auto));
+
+      var cells = workbook.Worksheets[0].Cells;
+      
+      var range = cells.CreateRange(0, 0, cells.LastCell.Row + 1, cells.LastCell.Column + 1);
+      
+      var exportOptions = new ExportRangeToJsonOptions();
+      
+      string json = JsonUtility.ExportRangeToJson(range, exportOptions);
+      
+      //System.IO.File.WriteAllText("output.json", jsonData);
+      
+      return json;
+    }
+
+    public static void GetCardsFromExcel(string excelFile, ref List<Card> cards)
+    {
+      var json = ExcelToJson(excelFile);
+
+      var intermediateCards = JsonConvert.DeserializeObject<List<JsonIntermediateCard>>(json);
+      
+      PopulateCardsFromJsonIntermediates(intermediateCards, ref cards);
+    }
+  }
+}
---
+++ TryJsonToObject/TryJsonToObject/Definitions.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+
+namespace TryJsonToObject
+{
+  public static class Guilds
+  {
+    public const string Borg = "Borg";
+    public const string Mech = "Mech";
+    public const string Mage = "Mage";
+    public const string Necro = "Necro";
+    public const string Neutral = "Neutral";
+
+    public static readonly List<string> All = new List<string>() { Borg, Mech, Mage, Necro, Neutral };
+  }
+
+  public static class Actions
+  {
+    public const string Attack = "A";
+    public const string Trade = "T";
+    public const string Draw = "D";
+    public const string Scrap = "S";
+    public const string Consume = "C";
+    public const string OpponentDiscard = "O";
+    public const string Heal = "H";
+
+    public static List<string> All = new List<string>() { Attack, Trade, Draw, Scrap, Consume, OpponentDiscard, Heal };
+  }
+
+  public class ActionSet
+  {
+    public int Attack { get; set; }
+    public int Trade { get; set; }
+    public int Draw { get; set; }
+    public int Scrap { get; set; }
+    public int Consume { get; set; }
+    public int OpponentDiscard { get; set; }
+    public int Heal { get; set; }
+  }
+  public static class CardTypes
+  {
+    public const string Unit = "Unit";
+    public const string Base = "Base";
+    public const string Unknown = "Unknown";
+
+    public static readonly List<string> All = new List<string>() { Unit, Base, Unknown };
+  }
+
+  public class Card
+  {
+    public Card() {}
+
+    public Card(
+      int id,
+      string name,
+      string type,
+      string guild,
+      int cost,
+      int defense,
+      int shield,
+      ActionSet defaultActions,
+      ActionSet guildActions,
+      ActionSet allyActions,
+      ActionSet scrapActions
+    )
+    {
+      Id = id;
+      Name = name;
+      Type = type;
+      Guild = guild;
+      Cost = cost;
+      Defense = defense;
+      Shield = shield;
+      DefaultAbilities = defaultActions;
+      GuildBonuses = guildActions;
+      AllyBonuses = allyActions;
+      ScrapBonuses = scrapActions;
+    }
+
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Guild { get; set; }
+    public int Cost { get; set; }
+    public int Defense { get; set; }
+    public int Shield { get; set; }
+    public ActionSet DefaultAbilities { get; set; }
+    public ActionSet GuildBonuses { get; set; }
+    public ActionSet AllyBonuses { get; set; }
+    public ActionSet ScrapBonuses { get; set; }
+    public int Id { get; set; }
+  }
+
+}
