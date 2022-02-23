9b5b29d56ca404c06365aef6f8092fc9cb60eada
Altamish Mahomed <gambit318@gmail.com>
2022-02-23 02:00:32 +0200

    Safety Commit.

--- ConsoleApp/ConsoleApp/Readers/EnemyReader.cs
+++ ConsoleApp/ConsoleApp/Readers/EnemyReader.cs
@@ -1,4 +1,6 @@
-﻿using Aspose.Cells;
+﻿using System;
+using System.Text;
+using Aspose.Cells;
 using MaM.Definitions;
 
 namespace MaM.Readers
@@ -88,6 +90,75 @@ namespace MaM.Readers
       }
 
       return names;
+    }
+
+    //TODO - refactor!
+    public static string GetEnemyName(Guild guild, ref EnemyNames enemyNames, ref Random random)
+    {
+      var buff = new StringBuilder();
+
+      var descriptor = "";
+
+      if (guild == Guilds.Neutral)
+      {
+        descriptor += enemyNames.neutralDescriptors[random.Next(0, enemyNames.neutralDescriptors.Count)];
+      }
+      else if (guild == Guilds.Borg)
+      {
+        descriptor += enemyNames.borgDescriptors[random.Next(0, enemyNames.borgDescriptors.Count)];
+      }
+      else if (guild == Guilds.Mech)
+      {
+        descriptor += enemyNames.mechDescriptors[random.Next(0, enemyNames.mechDescriptors.Count)];
+      }
+      else if (guild == Guilds.Mage)
+      {
+        descriptor += enemyNames.mageDescriptors[random.Next(0, enemyNames.mageDescriptors.Count)];
+      }
+      else if (guild == Guilds.Necro)
+      {
+        descriptor += enemyNames.necroDescriptors[random.Next(0, enemyNames.necroDescriptors.Count)];
+      }
+
+      var pre = enemyNames.pre[random.Next(0, enemyNames.pre.Count)];
+      var l = pre.Split(JoinedDelim);
+      if (l[0].ToLower() == "a")
+      {
+        pre = IsVowel(descriptor[0]) ? l[1] : l[0];
+      }
+
+      var collective = enemyNames.collective[random.Next(0, enemyNames.collective.Count)];
+
+      var post = enemyNames.post[random.Next(0, enemyNames.post.Count)];
+
+      var place = enemyNames.place[random.Next(0, enemyNames.place.Count)];
+
+      buff
+        .Append(pre.Trim())
+        .Append(' ')
+        .Append(descriptor.Trim())
+        .Append(' ')
+        .Append(collective.Trim())
+        .Append(' ')
+        .Append(post.Trim())
+        .Append(' ')
+        .Append(place.Trim())
+        .Append(' ')
+        ;
+
+      //AL.
+#if DEBUG
+      Console.WriteLine(buff);
+#endif
+      //
+
+      return buff.ToString();
+    }
+
+    private static bool IsVowel(char c)
+    {
+      return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+             c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
   }
 }
