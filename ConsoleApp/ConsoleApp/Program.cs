59a4051e56c1d9c015b50d992640e02aac7be588
Altamish Mahomed <gambit318@gmial.com>
2024-01-17 13:59:35 +0200

    Remove todo.

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -5,7 +5,5 @@ public static class Program
   private static void Main()
   {
     while (Menus.MainMenu.Show());
-
-//TODO - don't forget to revise all uses of 'ref' - only fo it if the value is gonna change or if the object is too large and a copy is not ideal.
   }
 }
